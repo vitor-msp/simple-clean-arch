b83381a2571225a740217a1455c5ad43920e7284
Vitor Mateus <vitor7jan@gmail.com>
2025-02-09 10:58:23 -0300

    improved factory

--- Factory/DependencyInjection.cs
+++ Factory/DependencyInjection.cs
@@ -1,13 +1,15 @@
-﻿using SimpleCleanArch.Application.Contract.UseCases;
-using SimpleCleanArch.Application.UseCases;
-using SimpleCleanArch.Application.Contract.Gateways;
-using SimpleCleanArch.Infra;
+﻿using SimpleCleanArch.Infra;
 using SimpleCleanArch.Domain.Contract;
 using SimpleCleanArch.Repository.Implementations;
 using SimpleCleanArch.Repository.Database.Context;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using SimpleCleanArch.Application.CreateProduct;
+using SimpleCleanArch.Application.GetProduct;
+using SimpleCleanArch.Application.UpdateProduct;
+using SimpleCleanArch.Application.DeleteProduct;
+using SimpleCleanArch.Application.Contract;
 
 namespace SimpleCleanArch.Factory;
 
@@ -15,13 +17,13 @@ public static class DependencyInjection
 {
     public static void AddDependencyInjections(this IServiceCollection services, IConfiguration configuration)
     {
-        var sqliteConnection = configuration.GetConnectionString("SqliteConnection") ?? "";
+        var sqliteConnection = configuration.GetConnectionString("SqliteConnection")
+            ?? throw new Exception("missing sqlite configuration");
         services.AddDbContext<ProductsContext>(options => options.UseSqlite(sqliteConnection));
         services.AddScoped<ICreateProduct, CreateProduct>();
         services.AddScoped<IGetProduct, GetProduct>();
         services.AddScoped<IUpdateProduct, UpdateProduct>();
         services.AddScoped<IDeleteProduct, DeleteProduct>();
-        // services.AddSingleton<IProductsRepository, ProductsRepositoryMemory>();
         services.AddScoped<IProductsRepository, ProductsRepositorySqlite>();
         services.AddScoped<ISendMailGateway, SendMailGateway>();
     }
