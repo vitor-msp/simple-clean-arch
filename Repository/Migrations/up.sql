eaa423142fa7628ec16ff1bc554b841a7f0f4505
Vitor Mateus <vitor7jan@gmail.com>
2025-04-19 14:45:29 -0300

    removed manual migrations

--- Repository/Migrations/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Products (
-    Id int not null primary key,
-    CreatedAt datetime not null,
-    Name varchar(10) not null,
-    Price decimal(5, 2) not null,
-    Description varchar(100),
-    Category varchar(10) 
-);
