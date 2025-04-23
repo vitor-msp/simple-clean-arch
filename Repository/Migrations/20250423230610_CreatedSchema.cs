15dc9a5ec34618ef5fbcb75ca660bf8f7f942a49
Vitor Mateus <vitor7jan@gmail.com>
2025-04-23 20:15:33 -0300

    added migration

---
+++ Repository/Migrations/20250423230610_CreatedSchema.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    price = table.Column<double>(type: "REAL", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    category = table.Column<string>(type: "TEXT", nullable: true),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "warehouses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_warehouses", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product_variants",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    sku = table.Column<string>(type: "TEXT", nullable: true),
+                    color = table.Column<int>(type: "INTEGER", nullable: false),
+                    size = table.Column<int>(type: "INTEGER", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    product_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product_variants", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_product_variants_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "inventories",
+                columns: table => new
+                {
+                    warehouse_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    product_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_inventories", x => new { x.warehouse_id, x.product_id });
+                    table.ForeignKey(
+                        name: "FK_inventories_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_inventories_warehouses_warehouse_id",
+                        column: x => x.warehouse_id,
+                        principalTable: "warehouses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "warehouse_details",
+                columns: table => new
+                {
+                    warehouse_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    city = table.Column<string>(type: "TEXT", nullable: true),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_warehouse_details", x => x.warehouse_id);
+                    table.ForeignKey(
+                        name: "FK_warehouse_details_warehouses_warehouse_id",
+                        column: x => x.warehouse_id,
+                        principalTable: "warehouses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "warehouse_transfers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    source_warehouse_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    target_warehouse_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    product_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_warehouse_transfers", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_warehouse_transfers_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_warehouse_transfers_warehouses_source_warehouse_id",
+                        column: x => x.source_warehouse_id,
+                        principalTable: "warehouses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_warehouse_transfers_warehouses_target_warehouse_id",
+                        column: x => x.target_warehouse_id,
+                        principalTable: "warehouses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "warehouse_transfer_details",
+                columns: table => new
+                {
+                    warehouse_transfer_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    product_quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    created_at = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_warehouse_transfer_details", x => x.warehouse_transfer_id);
+                    table.ForeignKey(
+                        name: "FK_warehouse_transfer_details_warehouse_transfers_warehouse_transfer_id",
+                        column: x => x.warehouse_transfer_id,
+                        principalTable: "warehouse_transfers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_inventories_product_id",
+                table: "inventories",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_variants_product_id",
+                table: "product_variants",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_variants_sku",
+                table: "product_variants",
+                column: "sku",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_name",
+                table: "products",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_warehouse_transfers_product_id",
+                table: "warehouse_transfers",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_warehouse_transfers_source_warehouse_id",
+                table: "warehouse_transfers",
+                column: "source_warehouse_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_warehouse_transfers_target_warehouse_id",
+                table: "warehouse_transfers",
+                column: "target_warehouse_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "inventories");
+
+            migrationBuilder.DropTable(
+                name: "product_variants");
+
+            migrationBuilder.DropTable(
+                name: "warehouse_details");
+
+            migrationBuilder.DropTable(
+                name: "warehouse_transfer_details");
+
+            migrationBuilder.DropTable(
+                name: "warehouse_transfers");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "warehouses");
+        }
+    }
+}
