227e16812da17d4de2e51ab1519430fb349aa52e
Vitor Mateus <vitor7jan@gmail.com>
2025-04-24 23:34:47 -0300

    added migration to update product variant

--- Repository/Migrations/20250425023419_UpdatedProductVariant.Designer.cs
+++ Repository/Migrations/20250425023419_UpdatedProductVariant.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleCleanArch.Repository.Context;
 
@@ -10,9 +11,11 @@ using SimpleCleanArch.Repository.Context;
 namespace Repository.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250425023419_UpdatedProductVariant")]
+    partial class UpdatedProductVariant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
